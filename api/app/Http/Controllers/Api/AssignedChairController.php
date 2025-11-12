b398e498a9c761eca67023b738ac58eb850ec5d8
alberto <albertodperez2000@gmail.com>
2025-11-12 17:43:47 -0400

    config

---
+++ api/app/Http/Controllers/Api/AssignedChairController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AssignedChair;
+use Illuminate\Http\Request;
+
+class AssignedChairController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AssignedChair $assignedChair)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AssignedChair $assignedChair)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AssignedChair $assignedChair)
+    {
+        //
+    }
+}
