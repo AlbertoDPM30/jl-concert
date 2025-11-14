f1c7b7b38527a48c2dd464c546e92dc3fae9950d
alberto <albertodperez2000@gmail.com>
2025-11-14 17:10:31 -0400

    front structure

--- app/src/app/page.jsx
+++ app/src/app/page.jsx
@@ -5,7 +5,11 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function HomeContent() {
   return (
     <main className="flex min-h-screen w-screen flex-col items-center justify-center bg-white dark:bg-amber-200 sm:items-center">
-      <div className="bg-white dark:bg-green-200 w-full h-full "></div>
+      <div className="bg-white dark:bg-green-200 grid grid-rows-4 gap-4">
+        <div className="">1</div>
+        <div className="">2</div>
+        <div className="">3</div>
+      </div>
     </main>
   );
 }
