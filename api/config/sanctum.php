dff82e29587ffc0974b8b42a32ad381f25098b47
alberto <albertodperez2000@gmail.com>
2025-11-17 14:22:02 -0400

    api cors config

--- api/config/sanctum.php
+++ api/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,eventseatassigment.xyz,app.eventseatassigment.xyz,dev.eventseatassigment.xyz,admin.eventseatassigment.xyz,148.230.85.230,148.230.85.230:3000,',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
